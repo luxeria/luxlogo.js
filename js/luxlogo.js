63f788778645016f0cebe53f029b8a8b21960f1b
n0ctu <contact@n1c0.ch>
2024-01-21 18:57:27 +0100

    Changed arrow from polygon to path

--- js/luxlogo.js
+++ js/luxlogo.js
@@ -83,7 +83,7 @@ class SVGHelper {
         return group;
     }
 
-    createClipPath(id, ...hrefs) {
+    createClipPathRefs(id, ...hrefs) {
         const clipPath = document.createElementNS("http://www.w3.org/2000/svg", "clipPath");
         clipPath.setAttribute("id", id);
         let i = 0;
@@ -95,7 +95,19 @@ class SVGHelper {
         return clipPath;
     }
 
-    createMask(id, ...hrefs) {
+    createMask(id, ...objects) {
+        const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+        mask.setAttribute("id", id);
+        let i = 0;
+        objects.forEach(object => {
+            mask.appendChild(object);
+            i++;
+        });
+
+        return mask;
+    }
+
+    createMaskRefs(id, ...hrefs) {
         const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
         mask.setAttribute("id", id);
         let i = 0;
@@ -123,6 +135,7 @@ class LuxLogo {
         this.svg = new SVGHelper("luxlogo_svg");
         this.size = 512;
         this.color1 = "#000000";
+        this.color2 = "#ffffff";
         this.rotation = 0;
         this.center = this.size / 2;
         this.numArrows = 3;
@@ -143,8 +156,8 @@ class LuxLogo {
 
     absCalc() {
         this.center = this.size / 2;
-        this.spacing = this.size * this.relSpacing / 100;
-        this.borderThickness = this.size * this.relBorderThickness / 100;
+        this.spacing = this.size * this.relSpacing / 1000;
+        this.borderThickness = this.size * this.relBorderThickness / 1000;
 
         this.innerCircleDiameter = this.size * this.relInnerCircleDiameter / 100;
         this.innerCircleRadius = this.innerCircleDiameter / 2;
@@ -156,7 +169,7 @@ class LuxLogo {
         this.arrowTipWidth = this.size * this.relArrowTipWidth / 100;
         this.arrowTipStart = this.center - this.size * this.relArrowTipStart / 100;
         this.arrowTipEnd = this.center - this.size * this.relArrowTipEnd / 100;
-        this.arrowNotchOffset = this.size * this.relArrowNotchOffset / 100;
+        this.arrowNotchOffset = this.size * this.relArrowNotchOffset / 1000;
         this.arrowBaseWidth = this.size * this.relArrowBaseWidth / 100;
         this.arrowBaseX = this.center - this.arrowBaseWidth / 2;
         const circleSegmentRadius = this.innerCircleDiameter / 2 + this.spacing;
@@ -166,29 +179,57 @@ class LuxLogo {
         this.arrowBaseY = this.center - this.innerCircleDiameter / 2 - this.spacing + this.arrowBaseOffsetCircleSegment ;
 
         // Outer Ring Part Calculations
+        // Todo: :(
+    }
 
+    // This would be a better way to create the outer ring part to make it compatible with Vector Graphics
+    // editors like Inkscape, but it is not working yet... Each point needs to be calculated separately...
+    createOuterRingPart(id) {
+        const outerRingPart = this.svg.createPath("outerRingPart", `
+            M ${this.center - this.outerCircleRadius} ${this.center}
+            A ${this.outerCircleRadius} ${this.outerCircleRadius} 0 0 1 ${this.center + this.outerCircleRadius} ${this.center}
+            L ${this.center + this.outerCircleRadius - this.outerCircleThickness} ${this.center}
+            A ${this.outerCircleRadius - this.outerCircleThickness} ${this.outerCircleRadius - this.outerCircleThickness} 0 0 0 ${this.center - this.outerCircleRadius + this.outerCircleThickness} ${this.center}
+            Z
+        `);
+        outerRingPart.setAttribute("id", id);
+        return outerRingPart;
+    }
+
+    // This is the ugly way to create the outer ring part, but it is working...
+    createOuterRingMask(id) {
+        const canvas = this.svg.createRectangle("canvas", 0, 0, this.size, this.size);
+        canvas.setAttribute("fill", "white");
+
+        const arrows = this.arrangeArrows("mask-arrows");
+        arrows.setAttribute("fill", "black");
+        arrows.setAttribute("stroke", "black");
+        arrows.setAttribute("stroke-width", this.spacing * 2);
+
+        const circle = this.svg.createCircle("mask-circle", this.center, this.center, this.outerCircleDiameter - this.outerCircleThickness * 2);
+        circle.setAttribute("fill", "black");
+        circle.setAttribute("stroke", "none");
+
+        return this.svg.createMask(id, canvas, arrows, circle);
     }
 
     createArrow(id) {
-        // Create path for arrow
-        const arrow = this.svg.createPath("arrow", `
+        const arrow = this.svg.createPath(id, `
             M ${this.center - this.arrowTipWidth / 2} ${this.arrowTipStart}
             L ${this.center} ${this.arrowTipEnd}
             L ${this.center + this.arrowTipWidth / 2} ${this.arrowTipStart}
             L ${this.center + this.arrowBaseWidth / 2} ${this.arrowTipStart - this.arrowNotchOffset}
             L ${this.center + this.arrowBaseWidth / 2} ${this.arrowBaseY}
-            A ${this.innerCircleRadius} ${this.innerCircleRadius} 0 0 0 ${this.center - this.arrowBaseWidth / 2} ${this.arrowBaseY}
+            A ${this.innerCircleRadius + this.spacing} ${this.innerCircleRadius + this.spacing} 0 0 0 ${this.center - this.arrowBaseWidth / 2} ${this.arrowBaseY}
             L ${this.center - this.arrowBaseWidth / 2} ${this.arrowTipStart - this.arrowNotchOffset}
             L ${this.center - this.arrowTipWidth / 2} ${this.arrowTipStart}
             Z
         `);
-        arrow.setAttribute("id", id);
-
         return arrow;
     }
 
-    arrangeArrows() {
-        const arrows = this.svg.createGroup("arrows");
+    arrangeArrows(id) {
+        const arrows = this.svg.createGroup(id);
         for (let i = 0; i < this.numArrows; i++) {
             const angle = (360 / this.numArrows) * i + this.rotation;
             const arrow = this.createArrow("arrow-" + (i+1));
@@ -198,63 +239,33 @@ class LuxLogo {
         return arrows;
     }
 
-    createOuterRingPart(id) {
-        const outerRingPart = this.svg.createPath("outerRingPart", `
-            M ${this.center - this.outerCircleRadius} ${this.center}
-            A ${this.outerCircleRadius} ${this.outerCircleRadius} 0 0 0 ${this.center + this.outerCircleRadius} ${this.center}
-            A ${this.outerCircleRadius} ${this.outerCircleRadius} 0 0 0 ${this.center - this.outerCircleRadius} ${this.center}
-            Z
-        `);
-        outerRingPart.setAttribute("id", id);
-        return outerRingPart;
-    }
-
     generate() {
 
-        // Calculate new absolute values
+        // Calculate new absolute values and clear the SVG
         this.absCalc();
-
-        console.log('LuxLogo.generate() called');
-
-        // Clear existing SVG contents
         this.svg.xml.innerHTML = "";
 
         // Set the new SVG viewBox size
         this.svg.xml.setAttribute("viewBox", `0 0 ${this.size} ${this.size}`);
 
-        // Add the inner circle
+        // Prepare elements
         const innerCircle = this.svg.createCircle("innerCircle", this.center, this.center, this.innerCircleDiameter);
-
-        // Add arrow group
-        const arrows = this.arrangeArrows();
-
-        // Add outer ring
-        const outerRingPart = this.createOuterRingPart("outerRingPart");
-
-        // Add arrows
-        //this.svg.appendChild(this.createArrowMask("arrowMask"));
-        //const arrows = this.arrangeArrows();
-        //this.svg.appendChild(arrows);
-
-        // Add outer ring
-        //this.svg.appendChild(this.createOuterRingMask("outerRingMask"));
-        //const outerRing = this.createCircle(this.center, this.center, this.outerCircleDiameter);
-        //outerRing.setAttribute("mask", "url(#outerRingMask)");
-        //this.svg.appendChild(outerRing);
-
-        // Colorize it
-        //this.svg.xml.setAttribute("fill", this.color1);
-        //this.svg.setAttribute("stroke", "black");
-        //this.svg.setAttribute("stroke-width", this.borderThickness);
+        const arrows = this.arrangeArrows("arrows");
+        const outerRingMask = this.createOuterRingMask("outerRingMask");
+        const outerRing = this.svg.createCircle("outerRing", this.center, this.center, this.outerCircleDiameter);
+        outerRing.setAttribute("mask", "url(#outerRingMask)");
+        //const outerRingPart = this.createOuterRingPart("outerRingPart");
 
         // Append elements to the SVG
         this.svg.xml.appendChild(innerCircle);
-        //this.svg.xml.appendChild(arrows);
-        this.svg.xml.appendChild(outerRingPart);
+        this.svg.xml.appendChild(arrows);
+        this.svg.xml.appendChild(outerRingMask);
+        this.svg.xml.appendChild(outerRing);
+        //this.svg.xml.appendChild(outerRingPart);
 
         // Colorize it
         this.svg.xml.setAttribute("fill", this.color1);
-        this.svg.xml.setAttribute("stroke", "red");
+        this.svg.xml.setAttribute("stroke", this.color2);
         this.svg.xml.setAttribute("stroke-width", this.borderThickness);
 
         // Return the SVG as a string
